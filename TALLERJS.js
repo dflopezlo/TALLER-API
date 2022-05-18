d6104137f1059dfc5ae3371c5a45eaf536ee4f00
ngiraldogi <ngiraldogi@unal.edu.co>
2022-05-17 23:09:17 -0500

    Se agrego una nueva grafica, en este caso de lineas, donde se indica la cantidad de feminicidios en el valle del cauca entre 2019 y 2020

--- TALLERJS.js
+++ TALLERJS.js
@@ -33,3 +33,84 @@ var layout = {
   
   Plotly.newPlot('grafico_torta', datosGraficasTorta, layout);
 });
+
+/******************************************************** Segunda gráfica ********************************************************/
+
+var municipios = [];
+var cantidad_2019 = [];
+var cantidad_2020 = [];
+
+let url= 'https://www.datos.gov.co/resource/fs93-tx8v.json';
+fetch(url)
+
+.then(datos_obtenidos=>datos_obtenidos.json())
+
+.then(function transformar (datos_obtenidos) {
+    datos_obtenidos.forEach(function agregar (datos_obtenidos) 
+    {
+        let pos = municipios.indexOf(datos_obtenidos.municipio); //verifico si el municipio ya lo guardé en el arreglo de municipios
+        if(pos == -1){
+            municipios.push(datos_obtenidos.municipio);
+            if(datos_obtenidos.a_o == 2019){
+                cantidad_2019.push(datos_obtenidos.cantidad);
+            }
+            else{
+                cantidad_2020.push(datos_obtenidos.cantidad);
+            }
+        }
+        else{
+            if(datos_obtenidos.a_o == 2019){
+                cantidad_2019[pos] = datos_obtenidos.cantidad;
+            }
+            else{
+                cantidad_2020[pos] = datos_obtenidos.cantidad;
+            }
+        }
+    });
+
+var linea1 = {
+    x: municipios,
+    y: cantidad_2019,
+    mode: 'lines',
+    name: 'Año 2019',
+    line: {
+        color: 'red',
+        width: 2
+    }
+};
+      
+var linea2 = {
+    x: municipios,
+    y: cantidad_2020,
+    mode: 'lines',
+    name: 'Año 2020',
+    line: {
+        color: 'blue',
+        width: 2
+    }
+};
+            
+var data = [ linea1, linea2 ];
+      
+var layout = {
+    title:'Total de Mujeres Víctimas de Feminicidio por Municipio en el Valle del Cauca, en los años 2019 y 2020',
+    height: 500,
+    width: 1500,
+    xaxis: {
+        title: 'Municipios del Valle del Cauca'
+    },
+    yaxis: {
+        title: 'Cantidad de Feminicidios'
+    },
+    legend: {
+        y: 0.5,
+        traceorder: 'reversed',
+        font: {
+          size: 16
+        }
+    }
+};
+      
+Plotly.newPlot('graficoLinea', data, layout);
+      
+});
